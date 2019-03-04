704e2e669a674f8b6c5c655758d06b0e04b67894
ChloePZS <chloe-pozas@hotmail.fr>
2019-03-04 12:04:00 +0100

    some tryings to clean items

--- scripts/item last cleaning.R
+++ scripts/item last cleaning.R
@@ -78,51 +78,86 @@ tax <- bind_rows(mad_per, haw_per, vir_per, mari_per) %>%
   rownames_to_column(var="site_code") %>%
   mutate(site_code = recode(site_code, "1" = "mad" , "2" = "haw","3" = "vir" , "4"="mari"))
 
-
       ##Cleaning !!
 
 #create a another data for removing items (just in case...)
 datatest <- data
+datatest$item_spname <- trimws(datatest$item_spname, which="both") #removing leading ad trailing whitespace
+
+datatest$item_spname[datatest$item_spname=="NA NA"] <- NA #replace NA NA by NA
+
+str(datatest$item_spname)
+
+
 #datatest$item_cor <- data$item_spname #get rid of the item_cor variable as was a mix of all items col
 
  datatest <- datatest %>% select(-item_spname) %>%
    rename(item_spname = item_cor)
  
- datatest$item_spname <- trimws(datatest$item_spname, which="both") #removing leading ad trailing whitespace
- 
- datatest$item_spname[datatest$item_spname=="NA NA"] <- NA
-
- str(datatest$item_spname)
 
     ##Discard unrelevant items row
+datatest$item_type <- as.factor(datatest$item_type) 
+unique(datatest$item_type)
+str(datatest$item_type)
+levels(datatest$item_type)[2] <- NA
+class(datatest$item_cor)
 
 unique(datatest$item_type)
+str(datatest$item_type)
+levels(datatest$item_fam_cor)[205] <- NA
+class(datatest$item_cor)
+
+unique(datatest$item_type)
+str(datatest$item_type)
+levels(datatest$item_ord_cor)[87] <- NA
+class(datatest$item_cor)
+
 
 datatest <- filter(datatest,!is.na(item_type)) #Remove items for which the type is NA  
 datatest <- filter(datatest,!is.na(item_cor) , item_kingdom %in% c("Animalia","Plantae")) #Remove row for which item = Na and kingdom Animalia or Plantae
 datatest <- filter(datatest %>% filter(!item_cor=="Gurry")) #remove gurry items
 datatest <- filter(datatest, !item_cor=="Animalia" & !item_cor=="Plantae")
 
+datatest_cor <- datatest %>%
+  mutate(item_cor=recode(item_cor,"Fish"="Actinopterygii")) %>% #rename 
+  filter(time!="night") %>%
+  mutate(lower_level = case_when(
+    !is.na(item_spname) ~ "Species",
+    item_cor==item_fam_cor ~ "Family", 
+    item_cor==item_ord_cor ~ "Order",
+    item_cor==item_class ~ "Class",
+    item_cor==item_phylum ~ "Phylum")) #create a variable of the last levels
 
-datatest_cor <- datatest %>% 
-  mutate(lower_level = ifelse(!is.na(item_spname), "Species", NA)) %>% 
-  mutate(lower_level)
+datatest_cor$lower_level
+ord <- c("Species","Family","Order","Class","Phylum")
+datatest_cor$lower_level <- factor(datatest_cor$lower_level, levels=ord, ordered=TRUE)
 
+class(datatest_cor$lower_level)
+
+min(datatest_cor$lower_level, na.rm=T)
+
+
+datatest_cor$lower_level_sp <- sapply(datatest_cor$fish_sp, function(x){
+  min(datatest_cor$lower_level)
+})
+
+
+class(datatest$item_cor)
+class(datatest$item_fam_cor)
+class(datatest$item_ord_cor)
+class(datatest$item_class)
+class(datatest$item_phylum)
+
+datatest$item_cor <- as.character(datatest$item_cor)
+datatest$item_fam_cor <- as.character(datatest$item_fam_cor)
+datatest$item_ord_cor <- as.character(datatest$item_ord_cor)
+datatest$item_class <- as.character(datatest$item_class)
+datatest$item_phylum <- as.character(datatest$item_phylum)
 
 datatest_cor <- datatest %>% 
   gather("item_kingdom", "item_phylum", "item_class", "item_ord_cor",
          "item_fam_cor", "item_gen_cor", "item_spname", key = "level", 
          value = "Tax_id")
-
-
-"Animalia" %in% datatest$item_kingdom
-
-NA %in% a$item_cor
- a <- datatest %>% filter(!item_cor=="Gurry")
- b <- datatest %>% filter(item_cor=="Gurry")
-
- 
- 
 
 
 
